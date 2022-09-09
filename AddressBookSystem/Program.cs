16060a76b658a4a867e53843f417fa1b1a38d55b
surajbridgelabz <surajnsinha@gmail.com>
2022-09-09 19:07:32 +0530

    [suraj] Add: Create a contact in AddressBook

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to AddressBook Problem Statement");
+            Contact contact = new Contact()
+            {
+                FirstName = "suraj",
+                LastName = "sinha",
+                Address = "Hadapsar",
+                City = "Pune",
+                State = "Maharashtra",
+                Zip = 411042,
+                Email = "xyz@gmail.com",
+            };
+            Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.Zip + "\n" + contact.Email);
         }
+    }
     }
 }
